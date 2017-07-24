3fc40e0ad8e688ed6a1902752e59b2c9c24a04df
dengzhihui <dengzhihui@funova.net>
2017-07-24 16:52:29 +0800

    test commnet

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ namespace hellowp
     {
         static void Main(string[] args)
         {
+            //comment test
             Console.WriteLine("BEEP!");
             Console.Beep(1000, 1000);
         }
