a6f2df8e34dc526de271a170767bd91979c8352e
dengzhihui <dengzhihui@funova.com>
2017-07-24 17:23:04 +0800

    comit second try!

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace hellowp
         {
             //comment test
             Console.WriteLine("BEEP!");
+            Console.WriteLine("测试新的工程!");
             Console.Beep(1000, 1000);
         }
     }
